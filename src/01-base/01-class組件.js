ddb459ecdef41daf30b0aa4255794dd5a0b6c239
stevecyj <ycsteven.tsao@gmail.com>
2022-04-03 12:54:41 +0800

    1st commit

---
+++ src/01-base/01-class組件.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+// class Test {
+//   constructor() {
+//     this.a = 1;
+//   }
+
+  // testa() {
+  //   console.log('test')
+  // }
+
+// }
+
+// class ChildTest extends Test{
+//   testb() {
+//     console.log('testb');
+//   }
+// }
+
+// let obj = new ChildTest();
+// obj.testa();
+// console.log(obj.a);
+
+class App extends React.Component{
+  render() {
+    return <div>Hello react component</div>
+  }
+}
+
+export default App;
